a6460b7ef0f5f3e867f7d4d694495760e84f9557
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-02-09 20:12:50 -0300

    conectando o firebase SDK do projeto com o back end do firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'whatsapp-clone';
+export class AppComponent implements OnInit {
+  title = 'instagram-clone';
+
+  ngOnInit(): void {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyDc4yaui4EucQ8JJXmW4gOR3bqKauJ8mpU',
+      authDomain: 'instagram-clone-95bed.firebaseapp.com',
+      projectId: 'instagram-clone-95bed',
+      storageBucket: 'instagram-clone-95bed.appspot.com',
+      messagingSenderId: '655851349140',
+      appId: '1:655851349140:web:9db49ec70643594e2d8081',
+    };
+
+    firebase.initializeApp(firebaseConfig);
+  }
 }
