ddc377c1f108ea825f06f2d2457c5a22bf61ecbe
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-02-11 19:32:03 -0300

    criacao e desenvolvimento do componente publicacoes

---
+++ src/app/home/publicacoes/publicacoes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-publicacoes',
+  templateUrl: './publicacoes.component.html',
+  styleUrls: ['./publicacoes.component.scss']
+})
+export class PublicacoesComponent {
+
+}
