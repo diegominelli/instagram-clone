aec157978be1d80edebb7f2082d31c7e96c56629
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-02-11 22:46:54 -0300

    efetuando o logout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Autenticacao } from './../autenticacao.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private autenticacao: Autenticacao) {}
 
+  ngOnInit(): void {}
+
+  public sair(): void {
+    this.autenticacao.sair();
+  }
 }
