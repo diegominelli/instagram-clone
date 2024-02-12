de9161394d4c05ba46b3f9a5db2ebf82181a5ede
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-02-11 23:11:12 -0300

    ajustando o modal de inclusao de publicacoes

--- src/app/home/incluir-publicacao/incluir-publicacao.component.ts
+++ src/app/home/incluir-publicacao/incluir-publicacao.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-incluir-publicacao',
   templateUrl: './incluir-publicacao.component.html',
-  styleUrls: ['./incluir-publicacao.component.scss']
+  styleUrls: ['./incluir-publicacao.component.scss'],
 })
-export class IncluirPublicacaoComponent {
+export class IncluirPublicacaoComponent implements OnInit {
+  public formulario: FormGroup = new FormGroup({
+    titulo: new FormControl(null),
+  });
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public publicar(): void {
+    console.log('chegamos até aqui!!!');
+  }
 }
