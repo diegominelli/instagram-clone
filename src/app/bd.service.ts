978cfaac20188d9ec5897166bf4c82a6aa0d6d04
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-02-14 20:18:30 -0300

    recuperando publicacoes do database

--- src/app/bd.service.ts
+++ src/app/bd.service.ts
@@ -39,4 +39,14 @@ export class Bd {
           );
       });
   }
+
+  public consultaPublicacoes(emailUsuario: string): any {
+    firebase
+      .database()
+      .ref(`publicacoes/${btoa(emailUsuario)}`)
+      .once('value')
+      .then((snapshot: any) => {
+        console.log(snapshot.val());
+      });
+  }
 }
--- src/app/home/publicacoes/publicacoes.component.ts
+++ src/app/home/publicacoes/publicacoes.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import { Bd } from 'src/app/bd.service';
 
 @Component({
   selector: 'app-publicacoes',
   templateUrl: './publicacoes.component.html',
-  styleUrls: ['./publicacoes.component.scss']
+  styleUrls: ['./publicacoes.component.scss'],
 })
-export class PublicacoesComponent {
+export class PublicacoesComponent implements OnInit {
+  public email?: string;
 
+  constructor(private bd: Bd) {}
+
+  ngOnInit(): void {
+    firebase.auth().onAuthStateChanged((user) => {
+      this.email = user?.email!;
+
+      this.atualizarTimeLine();
+    });
+  }
+
+  public atualizarTimeLine(): void {
+    this.bd.consultaPublicacoes(this.email!);
+  }
 }
