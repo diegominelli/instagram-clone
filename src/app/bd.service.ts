e64c22f5612960455746459c3a205b9d157c28f0
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-02-14 20:00:48 -0300

    ajustando a gravacao da imagem e titulo da publicacao

--- src/app/bd.service.ts
+++ src/app/bd.service.ts
@@ -9,33 +9,34 @@ export class Bd {
   public publicar(publicacao: any): void {
     console.log(publicacao);
 
-    let nomeImagem = Date.now();
-
     firebase
-      .storage()
-      .ref()
-      .child(`imagens/${nomeImagem}`)
-      .put(publicacao.imagem)
-      .on(
-        firebase.storage.TaskEvent.STATE_CHANGED,
-        (snapshot: any) => {
-          this.progresso.status = 'andamento';
-          this.progresso.estado = snapshot;
-          // console.log('Snapshot capturado no on()', snapshot);
-        },
-        (error) => {
-          this.progresso.status = 'erro';
-          // console.log(error);
-        },
-        () => {
-          this.progresso.status = 'concluido';
-          // console.log('upload completo');
-        }
-      );
+      .database()
+      .ref(`publicacoes/${btoa(publicacao.email)}`)
+      .push({ titulo: publicacao.titulo })
+      .then((resposta: any) => {
+        let nomeImagem = resposta.key;
 
-    // firebase
-    //   .database()
-    //   .ref(`publicacoes/${btoa(publicacao.email)}`)
-    //   .push({ titulo: publicacao.titulo });
+        firebase
+          .storage()
+          .ref()
+          .child(`imagens/${nomeImagem}`)
+          .put(publicacao.imagem)
+          .on(
+            firebase.storage.TaskEvent.STATE_CHANGED,
+            (snapshot: any) => {
+              this.progresso.status = 'andamento';
+              this.progresso.estado = snapshot;
+              // console.log('Snapshot capturado no on()', snapshot);
+            },
+            (error) => {
+              this.progresso.status = 'erro';
+              // console.log(error);
+            },
+            () => {
+              this.progresso.status = 'concluido';
+              // console.log('upload completo');
+            }
+          );
+      });
   }
 }
