9a062dc3dc7a46b674b6bbccf7c2d1654b32a43e
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-02-14 21:00:00 -0300

    listando as publicacoes na timeline

--- src/app/home/publicacoes/publicacoes.component.ts
+++ src/app/home/publicacoes/publicacoes.component.ts
@@ -24,6 +24,7 @@ export class PublicacoesComponent implements OnInit {
   public atualizarTimeLine(): void {
     this.bd.consultaPublicacoes(this.email!).then((publicacoes: any) => {
       this.publicacoes = publicacoes;
+      console.log(this.publicacoes);
     });
   }
 }
